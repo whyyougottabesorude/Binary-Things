d7b22297bb5d7241639ef0318bf8615577737885
whyyougottabesorude <yossansr06@gmail.com>
2019-07-08 18:25:24 +0700

    Fixed some bugs

--- final.cpp
+++ final.cpp
@@ -1,100 +1,17 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
 class Complement {
 public:
     int a[10], b[10], bit, result, complement1(int a[100], int bit), complement2(int a[100], int bit), plusMin(int b[100], int bit, int com);
-    void input1(), input2();
+    void input1(), input2(), menu();
 } c;
 
-void Complement::input1() {
-    cout << "Please input bit of biner : ";
-    cin >> c.bit;
-    string bin;
-    cout << "Please enter the biner : " << endl;
-    cin >> bin;
-    string temp;
-    for (int i = 0; i < c.bit; i++) {
-        temp = bin[i];
-        c.a[i] = stoi(temp);
-    }
-    system("clear");
-}
-
-void Complement::input2() {
-    cout << "Please input bit of biner : ";
-    cin >> c.bit;
-    string bin1, temp1, bin2, temp2;
-    cout << "Please enter the biner a: " << endl;
-    cin >> bin1;
-    for (int i = 0; i < c.bit; i++) {
-        temp1 = bin1[i];
-        c.a[i] = stoi(temp1);
-    }
-    system("clear");
-    cout << "Please enter the biner b: " << endl;
-    cin >> bin2;
-    for (int i = 0; i < c.bit; i++) {
-        temp2 = bin2[i];
-        c.b[i] = stoi(temp2);
-    }
-    system("clear");
-}
-
-int Complement::complement1(int a[100], int bit) {
-    c.result = 0;
-    int value;
-    if (c.bit == 4) {
-        value = 8;
-    }
-    else if (c.bit == 5) {
-        value = 16;
-    }
-
-    for (int i = 0; i < c.bit; i++) {
-        if (i == 0 && a[0] == 1) {
-            value -= 1;
-            c.result -= value;
-            value++;
-        }
-        else if (i != 0 && a[i] == 1) {
-            c.result += value;
-        }
-        value /= 2;
-    }
-    return c.result;
-}
-
-int Complement::complement2(int a[100], int bit) {
-    c.result = 0;
-    int value;
-    if (c.bit == 4) {
-        value = 8;
-    }
-    else if (c.bit == 5) {
-        value = 16;
-    }
-
-    for (int i = 0; i < c.bit; i++) {
-        if (i == 0 && a[0] == 1)
-        {
-            c.result -= value;
-        }
-        else if (i != 0 && a[i] == 1)
-        {
-            c.result += value;
-        }
-        value /= 2;
-    }
-    if (c.result == -8) {
-        c.result = 0;
-    }
-    return c.result;
-}
-
-int main() {
-awal:
+void Complement::menu() {
+    bool theEnd = false;
+    early:
     int choose, temp = 0;
     cout << "Choose menu : " << endl;
     cout << "1. 1's Complement to 2.s Complement" << endl;
@@ -107,67 +24,159 @@ awal:
 
     switch (choose) {
     case 1:
-        c.input1();
+        input1();
         cout << "1's Complement ";
-        c.complement1(c.a, c.bit);
-        for (int i = 0; i < c.bit; i++) {
-            cout << c.a[i];
+        complement1(a, bit);
+        for (int i = 0; i < bit; i++) {
+            cout << a[i];
         }
-        cout << " : " << c.result << " -> 2's Complement ";
-        c.complement2(c.a, c.bit);
-        for (int i = 0; i < c.bit; i++) {
-            cout << c.a[i];
+        cout << " : " << result << " -> 2's Complement ";
+        complement2(a, bit);
+        for (int i = 0; i < bit; i++) {
+            cout << a[i];
         }
-        cout << " : " << c.result << endl;
+        cout << " : " << result << endl;
         break;
 
     case 2:
-        c.input2();
-        c.complement2(c.a, c.bit);
-        temp = c.result;
-        c.complement2(c.b, c.bit);
-        temp += c.result;
-
-        for (int i = 0; i < c.bit; i++) {
-            cout << c.a[i];
+        input2();
+        complement2(a, bit);
+        temp = result;
+        complement2(b, bit);
+        temp += result;
+        
+        for (int i = 0; i < bit; i++) {
+            cout << a[i];
         }
 
-        cout << " " << c.complement2(c.a, c.bit) << " + ";
+        cout << " " << complement2(a, bit) << " + ";
 
-        for (int i = 0; i < c.bit; i++) {
-            cout << c.b[i];
+        for (int i = 0; i < bit; i++) {
+            cout << b[i];
         }
 
-        cout << " " << c.complement2(c.b, c.bit) << " = " << temp << endl
+        cout << " " << complement2(b, bit) << " = " << temp << endl
              << endl;
         break;
 
     case 3:
-        c.input2();
-        cout << c.complement2(c.a, c.bit) << endl;
-        temp = c.result;
-        cout << c.complement2(c.b, c.bit) << endl;
-        temp -= c.result;
-        cout << c.result << endl;
-        for (int i = 0; i < c.bit; i++) {
-            cout << c.a[i];
+        input2();
+        complement2(a, bit);
+        temp = result;
+        complement2(b, bit);
+        temp -= result;
+        for (int i = 0; i < bit; i++) {
+            cout << a[i];
         }
 
-        cout << " " << c.complement2(c.a, c.bit) << " - ";
+        cout << " " << complement2(a, bit) << " - ";
 
-        for (int i = 0; i < c.bit; i++) {
-            cout << c.b[i];
+        for (int i = 0; i < bit; i++) {
+            cout << b[i];
         }
 
-        cout << " " << c.complement2(c.b, c.bit) << " = " << temp << endl
+        cout << " " << complement2(b, bit) << " = " << temp << endl
              << endl;
         break;
 
     case 4:
-        return 0;
+        theEnd = true;
+        break;
 
     default:
         break;
     }
-    goto awal;
+    if (theEnd == false) {
+        goto early;
+    }
+
+}
+
+void Complement::input1() {
+    cout << "Please input bit of biner : ";
+    cin >> bit;
+    string bin;
+    cout << "Please enter the biner : " << endl;
+    cin >> bin;
+    string temp;
+    for (int i = 0; i < bit; i++) {
+        temp = bin[i];
+        a[i] = stoi(temp);
+    }
+    system("clear");
+}
+
+void Complement::input2() {
+    cout << "Please input bit of biner : ";
+    cin >> bit;
+    string bin1, temp1, bin2, temp2;
+    cout << "Please enter the biner a: " << endl;
+    cin >> bin1;
+    for (int i = 0; i < bit; i++) {
+        temp1 = bin1[i];
+        a[i] = stoi(temp1);
+    }
+    system("clear");
+    cout << "Please enter the biner b: " << endl;
+    cin >> bin2;
+    for (int i = 0; i < bit; i++) {
+        temp2 = bin2[i];
+        b[i] = stoi(temp2);
+    }
+    system("clear");
+}
+
+int Complement::complement1(int a[100], int bit) {
+    result = 0;
+    int value;
+    if(bit == 1) {
+        value = 1;
+    } else if (bit > 1) {
+        value = pow(2, bit - 1);
+    }
+
+    for (int i = 0; i < bit; i++) {
+        if (i == 0 && a[0] == 1) {
+            value -= 1;
+            result -= value;
+            value++;
+        }
+        else if (i != 0 && a[i] == 1) {
+            result += value;
+        }
+        value /= 2;
+    }
+    return result;
+}
+
+int Complement::complement2(int a[100], int bit) {
+    result = 0;
+    int value;
+    if(bit == 1) {
+        value = 1;
+    } else if (bit > 1) {
+        value = pow(2, bit - 1);
+    }
+
+    for (int i = 0; i < bit; i++) {
+        if (i == 0 && a[0] == 1)
+        {
+            result -= value;
+        }
+        else if (i != 0 && a[i] == 1)
+        {
+            result += value;
+        }
+        value /= 2;
+    }
+    if (result == -8) {
+        result = 0;
+    }
+    return result;
+}
+
+int main() {
+    c.menu();
+
+    return 0;
 }
